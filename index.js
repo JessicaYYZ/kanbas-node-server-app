30b6379891df6673a85a4ae2facffe4f6a3a4b0b
JessicaYYZ <jessicayuyiz@gmail.com>
2024-12-05 18:33:10 -0800

    mongodb

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import CourseUsersRoutes from "./Kanbas/Users/courseUsers.routes.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(
@@ -18,11 +23,13 @@ app.use(
     origin: process.env.NETLIFY_URL || "http://localhost:3000",
   })
 );
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
